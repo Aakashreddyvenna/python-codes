a2526590f6ff476fb4164292077e651a9ab397bd
Aakashreddyvenna <130352845+Aakashreddyvenna@users.noreply.github.com>
2023-08-02 22:41:29 +0530

    Add files via upload

---
+++ duckingtyping.py
@@ -0,0 +1,25 @@
+class ola:
+    
+    def details(self):
+        self.name="aakash"
+        self.age=19
+
+class Sypder:
+    
+    def excute(self):
+        print("spell check")
+        print("covenction check")
+        print("compling")
+        print("printing")
+class hola:
+    
+    def info(self,ide):
+        self.gender="M"
+        self.hair="Black"
+        ide.excute()
+        
+ide= Sypder()
+a1= ola()
+a2= hola()
+a1.details()
+a2.info(ide)
